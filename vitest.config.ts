b0a51952fadbfd6a9ebee5e72328b2de33fbbaa7
Nick K. <nick.kruchinin@gmail.com>
2025-07-13 20:43:44 +0300

    chore(test): Use default vitest settings for coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import {defineConfig} from "vitest/config"
 export default defineConfig({
   test: {
     include: ["tests/**/*.test.ts"],
-    reporters: ["default", "junit"],
-    outputFile: "./vitest-report.junit.xml",
     coverage: {
       include: ["src/**/*.ts"]
     }
